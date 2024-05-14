4a363a4cff20a7b5329a9490331f14f7746b4518
nukhbas <166951009+nukhbas@users.noreply.github.com>
2024-05-14 13:59:41 +0500

    optimized is_vowel function

--- Q4.py
+++ Q4.py
@@ -10,11 +10,13 @@
         
 def is_vowel(ch):
     
-    if len(ch) == 1 and ch == 'a' or ch == 'e' or ch == 'i' or ch == 'o' or ch == 'u':
+    if len(ch) == 1 and ch in ['a', 'e', 'i', 'o', 'u']:
         print ("True")
     else:
         print("False")
-(is_vowel('i'))
+
+
+is_vowel('i')
 
         
                
