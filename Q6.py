2d77155d92d900f92449c9a9ff7bdfe1a848ac03
nukhbas <166951009+nukhbas@users.noreply.github.com>
2024-05-14 13:19:01 +0500

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,18 @@
+def sum(number):
+ x = 0
+ for x in number:   
+    x = x + 1
+#  return x
+ 
+print(sum([2, 3, 4]))   
+
+def multiply(a, b):
+ x = 1
+ for x in a,b:
+        x = x * x 
+ return x
+
+print(multiply([ 3, 4]))
+
+    
+    
---
+++ Q7.py
@@ -0,0 +1,11 @@
+def reverse(x):
+  return x[::-1]
+
+txt = reverse("I am texting")
+
+print(txt)
+     
+
+
+
+
